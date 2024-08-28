1a0ff9179f96909ce2cd3174c8ec122237e6e8dd
Sancler <sanclerprodigy@gmail.com>
2024-08-28 17:06:40 -0300

    Exercicio 01 lista 04 string vazia.

--- Exercicios/JavaScritp/lib/ListaDeExercicios04/exercicio01.js
+++ Exercicios/JavaScritp/lib/ListaDeExercicios04/exercicio01.js
@@ -1,1 +1,16 @@
 //1. Verifique se uma string é vazia.//
+function integrantes(lista) {
+
+
+if (lista === "") {
+    console.log("true");
+} else { console.log("false");
+    
+}
+    
+}
+console.log(integrantes(""));
+console.log(integrantes("Hello"));
+console.log(integrantes(" "));
+
+
